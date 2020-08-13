170bcf3f3def41dc0e9b1e9599ffa2bad94268e5
Crowdbotics <team@crowdbotics.com>
2020-08-13 13:57:24 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,1 +1,15 @@
 import axios from "axios"
+import {
+  NEW_PLUGIN_HELLO_WORLD_USERNAME,
+  NEW_PLUGIN_HELLO_WORLD_PASSWORD
+} from "react-native-dotenv"
+const newPluginhelloworld = axios.create({
+  baseURL:
+    "https://crowdbotics-slack-dev.herokuapp.com/dashboard/app/8931/storyboard/9855/",
+  auth: {
+    username: NEW_PLUGIN_HELLO_WORLD_USERNAME,
+    password: NEW_PLUGIN_HELLO_WORLD_PASSWORD
+  },
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+export const apiService = {}
